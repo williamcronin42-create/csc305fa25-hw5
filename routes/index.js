4b963e801dcf999fa98e877910f7db894655b91b
williamcronin42-create <williamcronin42@gmail.com>
2025-11-19 23:17:36 -0500

    Update index.js
    Updated GET and POST routes to modify rendering behavior.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET request: show original form page
+router.get('/', function(req, res) {
+  res.render('index', { showFormOnly: true });
+});
+
+// POST request: show table + form without header
+router.post('/', function(req, res) {
+  res.render('index', { showFormOnly: false, formData: req.body });
 });
 
 module.exports = router;
